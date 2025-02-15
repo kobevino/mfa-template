527dd11632141fb9cd5759d45792430950404aa5
kobe <kobe8vino24@gmail.com>
2025-02-16 04:16:41 +0900

    chore: test

--- apps/_shell/src/App.tsx
+++ apps/_shell/src/App.tsx
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
 		element: (
 			<div>
 				Host application
-				<h1>Hello Kobe</h1>
+				<h1>Hello Kobe 123</h1>
 				<nav>
 					<Link to="/health">health</Link>
 					<br />
